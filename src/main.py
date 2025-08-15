ff4e7d1ae7a1fd1585fb016f48f5b54e28b71fe5
Aleksis <lesapond2007@gmail.com>
2025-08-15 17:03:32 +0300

    upd main.py

--- src/main.py
+++ src/main.py
@@ -7,49 +7,61 @@ from config import settings
 BASE_URL = "https://iss.moex.com/iss/statistics/engines/futures/markets/indicativerates/securities/CNY/RUB.json"
 
 
+def get_rate(url: str) -> float:
+    date_now = datetime.now()
+    year = date_now.year
+    month = date_now.month
+    day = date_now.day
+    params = [
+        ("lang", "ru"),
+        ("from", f"{year}-{month}-{day}"),
+        ("till", f"{year}-{month}-{day}"),
+        ("iss.meta", "off"),
+        ("iss.json", "extended"),
+        ("iss.meta", "off"),
+        ("limit", "100"),
+        ("start", "0"),
+        ("sort_order", "DESC"),
+        ("iss.meta", "off"),
+        ("iss.json", "extended"),
+        ("callback", "JSON_CALLBACK"),
+        ("lang", "ru"),
+    ]
+    response = requests.get(
+        BASE_URL,
+        params=params,
+    )
+    if response.status_code != 200:
+        print(response.status_code)
+        raise ConnectionError()
+    rate = float(response.json()[1].get("securities.current")[0].get("rate"))
+    return rate
+
+
+def send_to_api(rate: float) -> None:
+    api_url = settings.API_URL
+    params = {
+        "key": "current_rate",
+        "value": rate,
+    }
+    response = requests.patch(
+        api_url,
+        params=params,
+    )
+    if response.status_code != 200:
+        print(response.status_code)
+        raise ConnectionError("Api connection error")
+    print(f"curent_rate: {rate}")
+
+
 def main():
     while True:
-        date_now = datetime.now()
-        year = date_now.year
-        month = date_now.month
-        day = date_now.day
-        params = [
-            ("lang", "ru"),
-            ("from", f"{year}-{month}-{day}"),
-            ("till", f"{year}-{month}-{day}"),
-            ("iss.meta", "off"),
-            ("iss.json", "extended"),
-            ("iss.meta", "off"),
-            ("limit", "100"),
-            ("start", "0"),
-            ("sort_order", "DESC"),
-            ("iss.meta", "off"),
-            ("iss.json", "extended"),
-            ("callback", "JSON_CALLBACK"),
-            ("lang", "ru"),
-        ]
-        api_url = settings.API_URL
-        response = requests.get(
-            BASE_URL,
-            params=params,
-        )
-        if response.status_code != 200:
-            print(response.status_code)
-            raise ConnectionError()
-        rate = float(response.json()[1].get("securities.current")[0].get("rate"))
-        params = {
-            "key": "current_rate",
-            "value": rate,
-        }
-        response = requests.patch(
-            api_url,
-            params=params,
-        )
-        if response.status_code != 200:
-            print(response.status_code)
-            raise ConnectionError("Api connection error")
-        print(f"curent_rate: {rate}")
-        time.sleep(24 * 60 * 60)
+        try:
+            rate = get_rate(url=BASE_URL)
+            send_to_api(rate=rate)
+            time.sleep(24 * 60 * 60)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
